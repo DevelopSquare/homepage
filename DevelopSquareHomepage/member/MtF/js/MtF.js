bf0504a31489f5ff9f5bce546e46820f6ef216ba
FujisawaNoritaka <s1870262@ems.u-toyama.ac.jp>
2019-12-08 03:35:07 +0900

    「DevelopSquare　HomePage」の空白除去とmasterからの更新（と思われる）

---
+++ DevelopSquareHomepage/member/MtF/js/MtF.js
@@ -0,0 +1,3 @@
+/*var element = document.createElement("h1");
+var text = document.createTextNode("いい" +"お天気ですね");
+document.body.appendChild(element).appendChild(text);*/
--- sllider_sample/test.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($){
-    $('.slider').each(function(){
-		$(this).slick({
-			autoplay:true,
-			autoplaySpeed:5000,
-			dots:true,
-		});
-	});
-});
