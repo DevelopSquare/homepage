3137533a3c67a2ee52bb83b7a9591d28075b919d
FujisawaNoritaka <s1870262@ems.u-toyama.ac.jp>
2020-03-02 00:33:28 +0900

    member/index.htmlとjs修正

--- member/js/product_slidshow.js
+++ member/js/product_slidshow.js
@@ -11,31 +11,31 @@ jQuery(function($){
 
 document.getElementById("square-button").onclick = function() {
   this.classList.toggle("blue");
-  window.location.href = "../profile-KatoMori/html/index.html";
+  window.location.href = "profile-KatoMori/index.html";
 };
 
 document.getElementById("square-button2").onclick = function() {
   this.classList.toggle("blue");
-  window.location.href = "../profile-kicu/html/index.html";
+  window.location.href = "profile-kicu/index.html";
 };
 
 document.getElementById("square-button3").onclick = function() {
   this.classList.toggle("blue");
-  window.location.href = "../profile-MtF/html/index.html";
+  window.location.href = "profile-MtF/index.html";
 };
 
 document.getElementById("square-button4").onclick = function() {
   this.classList.toggle("blue");
-  window.location.href = "../profile-mori/html/index.html";
+  window.location.href = "profile-mori/index.html";
 };
 
 document.getElementById("square-button5").onclick = function() {
   this.classList.toggle("blue");
-  window.location.href = "../profile-takahashi1130/html/index.html";
+  window.location.href = "profile-takahashi1130/index.html";
 };
 
 document.getElementById("square-button6").onclick = function() {
   this.classList.toggle("blue");
-  
-  window.location.href = "../profile-Tora/html/index.html";
+
+  window.location.href = "profile-Tora/index.html";
 };
